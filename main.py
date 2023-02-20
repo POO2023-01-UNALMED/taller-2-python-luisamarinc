9e2b0d985f6931ea40bbcaae3a2cd90537f15574
Luisa <lumarinc@unal.edu.co>
2023-02-19 22:29:23 -0500

    taller finalizado

---
+++ main.py
@@ -0,0 +1,59 @@
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+    def cambiarColor(self, color):
+        if color == "verde" or "rojo" or "amarillo" or "negro" or "blanco":
+            self.color = color
+
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(self, registro):
+        self.registro =  registro
+    
+    def asignarTipo(self, tipo):
+        if tipo == "electrico" or "gasolina":
+            self.tipo = tipo
+
+class Auto:
+    cantidadCreado = 0
+    
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo= modelo
+        self.precio = precio
+        self.registro = registro
+        self.marca = marca
+        self.asientos = asientos
+        self.motor =  motor
+
+    def cantidadAsientos(self):
+        numeroAsientos = 0
+
+        for asiento in self.asientos:
+            if type(asiento) == Asiento:
+                numeroAsientos += 1
+        
+        return numeroAsientos
+
+    def verificarIntegridad(self):
+        if(self.registro == self.motor.registro):
+            for asiento in self.asientos:
+                if type(asiento) == Asiento:
+                    if asiento.registro != self.registro:
+                        return "Las piezas no son originales"
+            return "Auto original"
+        else:
+            return "Las piezas no son originales"                
+                
+        
+
+
+
+        
